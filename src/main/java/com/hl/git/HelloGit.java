c549f98f0c70e145c7c91c89d127ec47bd1b2169
ZhangYongfwi <1771368575@qq.com>
2020-09-21 15:05:34 +0800

    创建分支

--- src/main/java/com/hl/git/HelloGit.java
+++ src/main/java/com/hl/git/HelloGit.java
@@ -4,5 +4,6 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("hello git....");
 		System.out.println("进行更新的第一次....");
+		System.out.println("创建分支..");
 	}
 }
